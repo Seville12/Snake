2b3ae0fa90717bfeed2adcebaa57bdbd212ba0ad
Seville <seville12@mail.ru>
2019-02-14 18:28:27 +0400

    Реализовывает ViewModel (PlayerViewModel)

--- Snake/Controllers/PlayersController.cs
+++ Snake/Controllers/PlayersController.cs
@@ -67,6 +67,56 @@ namespace Snake.Controllers
             return View(player);
         }
 
+        //// GET: Players/Edit/5
+        //public async Task<IActionResult> Edit(int? id)
+        //{
+        //    if (id == null)
+        //    {
+        //        return NotFound();
+        //    }
+
+        //    var player = await _context.Player.FindAsync(id);
+        //    if (player == null)
+        //    {
+        //        return NotFound();
+        //    }
+        //    return View(player);
+        //}
+
+        //// POST: Players/Edit/5
+        //// To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        //// more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        //[HttpPost]
+        //[ValidateAntiForgeryToken]
+        //public async Task<IActionResult> Edit(int id, [Bind("Id,Name,Description")] Player player)
+        //{
+        //    if (id != player.Id)
+        //    {
+        //        return NotFound();
+        //    }
+
+        //    if (ModelState.IsValid)
+        //    {
+        //        try
+        //        {
+        //            _context.Update(player);
+        //            await _context.SaveChangesAsync();
+        //        }
+        //        catch (DbUpdateConcurrencyException)
+        //        {
+        //            if (!PlayerExists(player.Id))
+        //            {
+        //                return NotFound();
+        //            }
+        //            else
+        //            {
+        //                throw;
+        //            }
+        //        }
+        //        return RedirectToAction(nameof(Index));
+        //    }
+        //    return View(player);
+        //}
         // GET: Players/Edit/5
         public async Task<IActionResult> Edit(int? id)
         {
@@ -83,40 +133,20 @@ namespace Snake.Controllers
             return View(player);
         }
 
-        // POST: Players/Edit/5
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("Id,Name,Description")] Player player)
+        public ActionResult Edit(int id, [Bind("Id,Description")] PlayerViewModel playerModel)
         {
+            Player player =  _context.Player.Find(playerModel.Id);
             if (id != player.Id)
             {
                 return NotFound();
             }
-
-            if (ModelState.IsValid)
-            {
-                try
-                {
-                    _context.Update(player);
-                    await _context.SaveChangesAsync();
-                }
-                catch (DbUpdateConcurrencyException)
-                {
-                    if (!PlayerExists(player.Id))
-                    {
-                        return NotFound();
-                    }
-                    else
-                    {
-                        throw;
-                    }
-                }
-                return RedirectToAction(nameof(Index));
-            }
-            return View(player);
+            player.Description = playerModel.Description;
+            _context.Update(player);
+         _context.SaveChangesAsync();
+            return RedirectToAction("Index");
         }
+
 
         // GET: Players/Delete/5
         public async Task<IActionResult> Delete(int? id)
---
+++ Snake/Models/PlayerViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Snake.Models
+{
+    public class PlayerViewModel
+    {
+        public int Id { get; set; }
+        public string Description { get; set; }
+    }
+}
