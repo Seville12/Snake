13fdb87fdff8b8fc462c7633c3901ccd94676391
Seville <seville12@mail.ru>
2019-02-18 15:26:44 +0400

    Исправления согласно комментариям

--- Snake/Models/Player.cs
+++ Snake/Models/Player.cs
@@ -9,11 +9,7 @@ namespace Snake.Models
     public class Player
     {
         public int Id { get; set; }
-        [Required]
-        [StringLength(20, MinimumLength = 3, ErrorMessage = "Длина строки должна быть от 3 до 20 символов")]
         public string Name { get; set; }
-        [Required]
-        [StringLength(100, MinimumLength = 10, ErrorMessage = "Длина строки должна быть от 10 до 100 символов")]
         public string Description { get; set; }
     }
 }
--- Snake/Models/PlayerViewModel.cs
+++ Snake/Models/PlayerViewModel.cs
@@ -8,6 +8,7 @@ namespace Snake.Models
 {
     public class PlayerViewModel
     {
+        [Required]
         public int Id { get; set; }
         [Required]
         [StringLength(100, MinimumLength = 10, ErrorMessage = "Длина строки должна быть от 10 до 100 символов")]
